5db6dbdd715ddb020a8900c7fbe39b59744fccc7
MarceloAGD <marcelo.guerra@alumnos.ucn.cl>
2023-07-19 03:16:28 -0400

    feat:(module) update post and community

--- src/neo4j-comm/neo4j-comm.module.ts
+++ src/neo4j-comm/neo4j-comm.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { Neo4jCommService } from './neo4j-comm.service';
 import { Neo4jCommResolver } from './neo4j-comm.resolver';
+import { Neo4jCommRepository } from './neo4j-comm.repository';
 
 @Module({
-  providers: [Neo4jCommService, Neo4jCommResolver]
+  providers: [Neo4jCommService, Neo4jCommResolver, Neo4jCommRepository],
+  exports: [Neo4jCommRepository]
 })
 export class Neo4jCommModule {}
--- src/neo4j-post/neo4j-post.module.ts
+++ src/neo4j-post/neo4j-post.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { Neo4jPostService } from './neo4j-post.service';
 import { Neo4jPostResolver } from './neo4j-post.resolver';
+import { Neo4jPostRepository } from './neo4j-post.repository';
 
 @Module({
-  providers: [Neo4jPostService, Neo4jPostResolver]
+  providers: [Neo4jPostService, Neo4jPostResolver, Neo4jPostRepository],
+  exports: [Neo4jPostRepository]
 })
 export class Neo4jPostModule {}
